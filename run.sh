d30585c0b50c53e3d180f72b118b78ce967b15ef
wangzhesun <wangzhe.sun@outlook.com>
2022-08-28 16:21:11 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,31 +1,11 @@
 # base learning step
-DATA_ROOT=../data
-BATCH=24
-DATASET=voc
-NAME=MiB
-TASK=15-1-split1
-STEP=0
-LR=0.01
-EPOCH=30 #30
-METHOD=MiB
-NUMSHOT=5
-NUMRUN=5 #5
-
-
-python -m torch.distributed.launch --nproc_per_node=1 run.py --data_root ${DATA_ROOT}  \
-       --batch_size ${BATCH} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step ${STEP} \
-       --lr ${LR} --epochs ${EPOCH} --method ${METHOD}
-
-
-
-# continual learning step
 #DATA_ROOT=../data
 #BATCH=24
 #DATASET=voc
 #NAME=MiB
-#TASK=15-1-split2
-#STEP=1
-#LR=0.001
+#TASK=15-1-split1
+#STEP=0
+#LR=0.01
 #EPOCH=30 #30
 #METHOD=MiB
 #NUMSHOT=5
@@ -34,6 +14,26 @@ python -m torch.distributed.launch --nproc_per_node=1 run.py --data_root ${DATA_
 #
 #python -m torch.distributed.launch --nproc_per_node=1 run.py --data_root ${DATA_ROOT}  \
 #       --batch_size ${BATCH} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step ${STEP} \
-#       --lr ${LR} --epochs ${EPOCH} --method ${METHOD} --num_shot ${NUMSHOT} --num_runs ${NUMRUN} \
-#       --few_shot True --all_step True
-#       # comment two entries in the last line if do not want few shot or wamt to run each step separately
+#       --lr ${LR} --epochs ${EPOCH} --method ${METHOD}
+
+
+
+# continual learning step
+DATA_ROOT=../data
+BATCH=24
+DATASET=voc
+NAME=MiB
+TASK=15-1-split2
+STEP=1
+LR=0.001
+EPOCH=30 #30
+METHOD=MiB
+NUMSHOT=5
+NUMRUN=5 #5
+
+
+python -m torch.distributed.launch --nproc_per_node=1 run.py --data_root ${DATA_ROOT}  \
+       --batch_size ${BATCH} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step ${STEP} \
+       --lr ${LR} --epochs ${EPOCH} --method ${METHOD} --num_shot ${NUMSHOT} --num_runs ${NUMRUN} \
+       --few_shot True --all_step True
+       # comment two entries in the last line if do not want few shot or wamt to run each step separately
