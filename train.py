dad2e716e6966148836ce02e7509841fef7abad8
wangzhesun <wangzhe.sun@outlook.com>
2022-09-11 14:03:10 -0500

    Update train.py

--- train.py
+++ train.py
@@ -137,8 +137,12 @@ class Trainer:
             # xxx first backprop of previous loss (compute the gradients for regularization methods)
             loss_tot = loss + lkd + lde + l_icarl
 
-            with amp.scale_loss(loss_tot, optim) as scaled_loss:
-                scaled_loss.backward()
+
+            #################################################################################
+            # with amp.scale_loss(loss_tot, optim) as scaled_loss:
+            #     scaled_loss.backward()
+            loss_tot.backward()
+            #################################################################################
 
             # xxx Regularizer (EWC, RW, PI)
             if self.regularizer_flag:
