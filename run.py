70ce05199a3bdb23d3550dea78f73f8f19fc655c
wangzhesun <wangzhe.sun@outlook.com>
2022-09-12 01:14:54 -0500

    Update run.py

--- run.py
+++ run.py
@@ -207,14 +207,14 @@ def main(opts):
     print('\n printing max and min index 0 and 1 test image: ')
     print(test_dst[0][0].size())
     print(test_dst[1][0].size())
-    print(torch.max(test_dst[0][0]))
-    print(torch.min(test_dst[0][0]))
+    print(torch.max(test_dst[17][0]))
+    print(torch.min(test_dst[17][0]))
     print(test_dst[0][0].is_cuda)
     print('\n printing max and min index 0 and 1 test label: ')
     print(test_dst[0][1].size())
     print(test_dst[1][1].size())
-    print(torch.max(test_dst[0][1]))
-    print(torch.min(test_dst[0][1]))
+    print(torch.max(test_dst[17][1]))
+    print(torch.min(test_dst[17][1]))
     print(test_dst[0][1].is_cuda)
     ##################################################################################
 
